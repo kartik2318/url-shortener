7b81ee4aaa24808b2ee316b2006c40c37de81992
kartik2318 <kartikpatare1823@gmail.com>
2025-11-20 19:48:21 +0530

    Added the login button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -37,6 +37,13 @@ export default function Home() {
         SimpleShort <span className="text-blue-600">— Shorten URLs Instantly</span>
       </h1>
 
+      <Link
+            className="px-5 py-2.5 bg-green-600 hover:bg-green-700 text-white font-medium rounded-xl transition shadow-sm"
+            href="/login"
+            >
+              Login
+            </Link>
+
       {/* CARD CONTAINER */}
       <div className="bg-white rounded-2xl shadow-lg p-6 border border-gray-100">
         <form onSubmit={handleCreate} className="space-y-4">
